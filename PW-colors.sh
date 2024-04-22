abaaeb6685900e5ed53ddc5c00ba0d782c10f7a7
chandu priya <chandupriya.pul@gmail.com>
2024-04-22 13:46:40 +0530

    variable-01

---
+++ PW-colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RED="/e[31m"
+GREEN="/e[32m"
+LR="/e[1;91m"
+N="/e[0m"
+echo "Please Enter User-Name: "
+
+read -s USERNAME 
+echo "Please Enter Passward:"
+read -s PASSWARD
+
+echo "User-Nameis: $RED {$USERNAME} $N and Passward is:$LR  {$PASSWARD} $N"
