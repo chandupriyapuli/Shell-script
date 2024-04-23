715e79ab4d94b1c53c5ffcf0eb472d0de6777636
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 12:31:56 +0530

    variable-01

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -6,6 +6,8 @@ NUM2=$2
 if ["$NUM1" -gt "$NUM2"];
 then
    echo " The number $NUM1 is Greater Than $NUM2"
+   else
+   echo " The number $NUM1 is Less Than Than $NUM2"
 
-   fi
+fi
    # -gt -lt -eq -ge -le
