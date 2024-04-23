cb27581c62881c3ac85698e9fb8e77b53a226fc6
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 11:47:37 +0530

    variable-01

--- 04-array.sh
+++ 04-array.sh
@@ -8,3 +8,8 @@ echo "Tha First name of Array is:${names[1]}"
 echo ${names[@]} # Print All Arguments using " @ " (like sindu, siva, 67, priya)
 
 echo ${names[*]}  # Print All Arguments using " * " (like sindu, siva, 67, priya)
+
+# To print a selected index element
+selected_index=3
+echo “Selected index element at index $selected_index:”
+echo “${names[$selected_index]}”
