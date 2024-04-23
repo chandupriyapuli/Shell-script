911e49ba15084da46030294507ef66f93575ba55
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 12:38:50 +0530

    variable-01

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+NUM1=$1
+#NUM2=$2
+
+if [ $NUM1 -gt 10 ];
+then
+   echo " The number $NUM1 is Greater Than 10" 
+
+fi
+   # -gt -lt -eq -ge -le
+
+   #!/bin/bash
+
 NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
