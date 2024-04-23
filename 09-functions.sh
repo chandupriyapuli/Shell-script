e447f617f9a9911d653ca8108f1087a1b8dfeafb
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 16:34:13 +0530

    variable-01

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,10 +1,33 @@
 
 #!/bin/bash
 
-ADD-FUN()
-{
-    NO1=$1
-    NO2=$2
-    echo " $(($NO1+$NO2))"
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
-ADD-FUN "$1" "$2"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
