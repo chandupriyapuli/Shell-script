b9490fca26da510273893862921ad4e802bb8db6
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 11:34:17 +0530

    variable-01

--- 04-array.sh
+++ 04-array.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # declare static Array
-MY-ARRAY=("1" "priya" "chandu" "siva" "kumar")
-echo ${MY-ARRAY[@]}
+MOVIES=("RRR" "DjTillu" "murari")
+
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[@]}"
