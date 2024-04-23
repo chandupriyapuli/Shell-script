ba27d76bc2f5fc90bd02cda4fafe69dd12c1e829
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 16:20:33 +0530

    variable-01

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,10 +1,7 @@
+
 #!/bin/bash
 
-# Define a function that returns a value
-add() {
-    echo "$(($1 + $2))"
-}
-
-# Call the function and capture the return value
-result=$((add($a+$b)))
-echo "The result is: $result"
+a=$1
+b=$2
+p=$(($a*$b))
+echo "The product of $a and $b = $p"
