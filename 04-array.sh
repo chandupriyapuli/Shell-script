c8af11c024ddaf35d4836d55c4297728ca8cd4f0
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 11:50:31 +0530

    variable-01

--- 04-array.sh
+++ 04-array.sh
@@ -13,3 +13,13 @@ echo ${names[*]}  # Print All Arguments using " * " (like sindu, siva, 67, priya
 selected_index=3
 echo “Selected index element at index $selected_index:”
 echo “${names[$selected_index]}”
+
+# To print elements from a particular index
+echo “Elements from a particular index:”
+echo “${names[@]:2}” # Prints elements starting from index 2
+echo “${names[*]:2}” # Prints elements starting from index 2
+
+# To print elements in a range
+echo “Elements in a range:”
+echo “${names[@]:1:3}” # Prints elements from index 1 to 3
+echo “${names[*]:1:3}” # Prints elements from index 1 to 3
