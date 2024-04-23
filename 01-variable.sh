0e14fb34b8e2c09cb7e190c4ed1459c06a5bc908
chandu priya <chandupriya.pul@gmail.com>
2024-04-23 11:14:14 +0530

    variable-01

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,5 +1,8 @@
 PERSON1=$1 # Here it is take the value from execution place
 PERSON2=$2
 
+PERSON3=ramesh
+
 echo "my name is : $PERSON1" # if we didn't put $ in front of PERSON SHELL will not take it as a variable.
 echo "my brodher name is : $PERSON2"
+echo "${PERSON3}, he is working at Capgemini."
