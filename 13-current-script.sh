dae79cb1b5c382767184ea0dd1060c05c95b87f7
chandu priya <chandupriya.pul@gmail.com>
2024-04-25 12:25:33 +0530

    variable-01

--- 13-current-script.sh
+++ 13-current-script.sh
@@ -23,9 +23,10 @@ DOT()
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
+./14-other-script.sh
 
 #source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
 }
+DOT
