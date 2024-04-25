4b1e2151f2d9cef1970fc3f96814e147e90b8a2c
chandu priya <chandupriya.pul@gmail.com>
2024-04-25 12:24:24 +0530

    variable-01

--- 13-current-script.sh
+++ 13-current-script.sh
@@ -15,3 +15,17 @@ read num2
 sum=$(ADD $num1 $num2)
 
 echo "Adding Numbers Are $num1 and $num2 total is: $sum"
+
+DOT()
+{
+    COURSE="DevOps from Current Script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+./16-other-script.sh
+
+#source ./16-other-script.sh
+
+echo "After calling other script, course: $COURSE"
+}
--- 14-other-script.sh
+++ 14-other-script.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+COURSE="DevOps from Other Script"
+
+echo "Variable value from Other script: $COURSE"
+echo "Process ID of other script: $$"
